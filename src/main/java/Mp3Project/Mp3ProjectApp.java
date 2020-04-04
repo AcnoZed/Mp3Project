e78e0a8d0ebb4e6237a808c978ba0ef4971729f7
AcnoZed <Me.ouss.am@outlook.de>
2020-04-04 20:54:44 +0200

    Added mp3File

--- src/main/java/Mp3Project/Mp3ProjectApp.java
+++ src/main/java/Mp3Project/Mp3ProjectApp.java
@@ -1,11 +1,16 @@
 package Mp3Project;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Mp3ProjectApp {
 
     public static void main(String[] args) throws Exception {
-
+      checkArgument(args);
+      ReadInFile readInFile = new ReadInFile();
+      List<String> content = readInFile.readFile(args[0]);
+      content.forEach(System.out::println);
     }
 
     public static void checkArgument(String[] argv) throws Exception {
