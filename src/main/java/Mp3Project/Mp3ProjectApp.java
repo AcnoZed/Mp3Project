61a947863dac37a57093edef1dd0433d3e8f59a8
AcnoZed <Me.ouss.am@outlook.de>
2020-04-04 21:15:47 +0200

    Saving PathNames

--- src/main/java/Mp3Project/Mp3ProjectApp.java
+++ src/main/java/Mp3Project/Mp3ProjectApp.java
@@ -1,7 +1,6 @@
 package Mp3Project;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 public class Mp3ProjectApp {
@@ -9,14 +8,14 @@ public class Mp3ProjectApp {
     public static void main(String[] args) throws Exception {
       checkArgument(args);
       ReadInFile readInFile = new ReadInFile();
-      List<String> content = readInFile.readFile(args[0]);
+      List<Path> content = readInFile.readFile(args[0]);
       content.forEach(System.out::println);
     }
 
-    public static void checkArgument(String[] argv) throws Exception {
+    private static void checkArgument(String[] argv) throws Exception {
       if(argv.length != 1){
         System.out.println("-----------------------------");
-        System.out.println("Missing Argument");
+        System.out.println("You need to specify a valid mp3 directory");
         System.out.println("-----------------------------");
         throw new Exception();
       }
--- src/main/java/Mp3Project/ReadInFile.java
+++ src/main/java/Mp3Project/ReadInFile.java
@@ -1,28 +1,39 @@
 package Mp3Project;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 class ReadInFile {
 
-  List<String> readFile(String path) throws Exception {
-    if(validateFilePath(path)){
-      Path pathFile = Paths.get(path);
-      List<String> contents = Files.readAllLines(pathFile);
-      return contents;
+  List<Path> readFile(String fileDirectory) throws Exception {
+    List<Path> files = new ArrayList<>();
+    if(validateFilePath(fileDirectory)){
+      Path path = Paths.get(fileDirectory);
+      try {
+        DirectoryStream<Path> stream = Files.newDirectoryStream(path,"*.mp3");
+        for(Path file: stream){
+          files.add(file.getFileName());
+        }
+      }catch (IOException ignored){
+
+      }
+      return files;
     } else {
       System.out.println("-----------------------------");
       System.out.println("PathValidation failed!");
       System.out.println("-----------------------------");
-      throw new Exception();
+      throw new Exception("The specified directory does not exist : " + fileDirectory);
     }
   }
 
-  private boolean validateFilePath(String path) {
-    Path pathFile = Paths.get(path);
-    return Files.exists(pathFile, LinkOption.NOFOLLOW_LINKS);
+  private boolean validateFilePath(String fileDirectory) {
+    Path path = Paths.get(fileDirectory);
+    return Files.exists(path, LinkOption.NOFOLLOW_LINKS);
   }
 }
